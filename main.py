a8ae641e604653bd48d81a71aba89067bfbbb81a
sharathprakash81 <sharathprakash81@gmail.com>
2022-12-27 16:06:21 +0000

    "Data Ingestion Component completion"

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from sensor.logger import logging
 from sensor.exception import SensorException
 from sensor.utils import get_collection_as_dataframe
 from sensor.entity import config_entity
-#from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_ingestion import DataIngestion
 import sys
 import os
 
