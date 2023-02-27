82a627fd3e3970860926d6ee87e54c723a679f2d
sharathprakash81 <sharathprakash81@gmail.com>
2023-02-27 23:22:10 +0000

    Deploy final

--- airflow/dags/training_pipeline.py
+++ airflow/dags/training_pipeline.py
@@ -2,20 +2,22 @@ from asyncio import tasks
 import json
 from textwrap import dedent
 import pendulum
+import os
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-import os, sys
+
 
 with DAG(
     'sensor_training',
-    default_args={'retries':2},
+    default_args={'retries': 2},
+    # [END default_args]
     description='Sensor Fault Detection',
     schedule_interval="@weekly",
-    start_date=pendulum.datetime(2023, 2, 27, tz="UTC"),
-    catchup= False,
-    tags=['example']
-       
+    start_date=pendulum.datetime(2022, 12, 11, tz="UTC"),
+    catchup=False,
+    tags=['example'],
 ) as dag:
+
     
     def training(**kwargs):
         from sensor.pipeline.training_pipeline import start_training_pipeline
@@ -23,11 +25,19 @@ with DAG(
     
     def sync_artifact_to_s3_bucket(**kwargs):
         bucket_name = os.getenv("BUCKET_NAME")
-        os.system(f"aws s3 sync /app/artifact s3://{bucket_name}//artifacts")
+        os.system(f"aws s3 sync /app/artifact s3://{bucket_name}/artifacts")
         os.system(f"aws s3 sync /app/saved_models s3://{bucket_name}/saved_models")
-        
-    training_pipeline = PythonOperator(task_id="train_pipeline",python_callable=training)
-    
-    sync_data_to_s3 = PythonOperator(task_id="sync_data_to_s3",python_callable=sync_artifact_to_s3_bucket)
+
+    training_pipeline  = PythonOperator(
+            task_id="train_pipeline",
+            python_callable=training
+
+    )
+
+    sync_data_to_s3 = PythonOperator(
+            task_id="sync_data_to_s3",
+            python_callable=sync_artifact_to_s3_bucket
+
+    )
 
     training_pipeline >> sync_data_to_s3
